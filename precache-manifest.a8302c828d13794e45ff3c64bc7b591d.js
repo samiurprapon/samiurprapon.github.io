46671ce5a1bc5e85135f2800af2af49352e0e6d7
Samiur Prapon <samiur.prapon@gmail.com>
2021-01-29 06:19:38 +0600

    Updates

---
+++ precache-manifest.a8302c828d13794e45ff3c64bc7b591d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e5a100c5183996a2f31178bcedf548e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "964acf61af0b2a0df487",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "e949aed2841dd929f46d",
+    "url": "/static/css/4.19dfc3d0.chunk.css"
+  },
+  {
+    "revision": "55b6fcc4307375bc7b4e",
+    "url": "/static/css/main.87de27d1.chunk.css"
+  },
+  {
+    "revision": "4b9ec4008e050c4aeed1",
+    "url": "/static/js/2.6ebaae20.chunk.js"
+  },
+  {
+    "revision": "92cf806d0e2b0afd06d3bb0775715cc1",
+    "url": "/static/js/2.6ebaae20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "964acf61af0b2a0df487",
+    "url": "/static/js/3.c5203209.chunk.js"
+  },
+  {
+    "revision": "e949aed2841dd929f46d",
+    "url": "/static/js/4.2745d275.chunk.js"
+  },
+  {
+    "revision": "55b6fcc4307375bc7b4e",
+    "url": "/static/js/main.ad512b91.chunk.js"
+  },
+  {
+    "revision": "7c5557c9bae1561074f1",
+    "url": "/static/js/runtime-main.ee5fb14d.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4e551bf664e901f59d5a648262e7bbca",
+    "url": "/static/media/Logo_of_Carmichael_College,_Rangpur.4e551bf6.png"
+  },
+  {
+    "revision": "32691c57c5bd14fcb651ece18a8491b0",
+    "url": "/static/media/Logo_of_Rangpur_Zilla_School.32691c57.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "c6c6af0127f199356f795ef521477d99",
+    "url": "/static/media/northsouthLogo.c6c6af01.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  }
+]);
